a9916484a3826519ead9d416a6abe6802cbb1993
Matheus Luchsinger <102822195+luquisinger@users.noreply.github.com>
2023-07-05 09:53:17 -0300

    email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { fadeIn } from '../variants';
 import {motion} from 'framer-motion'
-
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] =useState('')
+
+  function sendEmail(e){
+    e.preventDefault()
+
+    if(name === '' || email === '' || message === ''){
+      alert('Preencha todos os campos.')
+      return
+    }
+
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email
+    }
+    emailjs.send("service_x0fry1b", "template_omivk84", templateParams, "uk5UMfRDB-l86-xmx")
+    .then((response) => {
+      console.log("EMAIL ENVIADO", response.status, response.text)
+      setName('')
+      setMessage('')
+      setEmail('')
+    }, (err) => console.log("erro", err))
+  }
   return (
     <section className='py-16 lg:section' id='contact'>
       <div className='container mx-auto'>
@@ -33,24 +58,31 @@ const Contact = () => {
             whileInView={'show'}
             viewport={{once: false, amount: 0.3 }}
             className='flex-1 border rounded-2x1 flex flex-col gap-y-6
-          pb-24  p-6 items-start'>
+          pb-24  p-6 items-start'
+            onSubmit={sendEmail}>
           <input 
                 className='bg-transparent border-b py-3 outline-none w-full
                 placeholder:text-white focus:border-accent transition-all'
                 type='text' 
                 placeholder='Seu nome'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
               <input 
                 className='bg-transparent border-b py-3 outline-none w-full
                 placeholder:text-white focus:border-accent transition-all'
                 type='text' 
                 placeholder='Seu email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <textarea 
                 className='bg-transparent border-b py-3 outline-none w-full
                 placeholder:text-white focus:border-accent transition-all
                 resize-none mb-12'
                 placeholder='Sua mensagem'
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
               />
               <button className='btn btn-lg'>Enviar mensagem</button>
           </motion.form>
