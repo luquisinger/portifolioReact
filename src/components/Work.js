55fec7fb284f30d7100048a683986a78ec9929ae
Matheus Luchsinger <102822195+luquisinger@users.noreply.github.com>
2024-01-29 17:19:08 -0300

    Update dos projetos

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { motion } from 'framer-motion'
 import Img1 from '../assets/p1.jpg'
 import Img2 from '../assets/p2.jpg'
-import Img3 from '../assets/p3.jpg'
+import Img3 from '../assets/p3.png'
 import { fadeIn } from '../variants';
 
 
@@ -51,7 +51,7 @@ const Work = () => {
                 <div className='absolute -bottom-full left-12 group-hover:bottom-14 
             transition-all duration-700 z-50'>
                   <span className='text-3x1 text-white'>
-                    Site de filmes.
+                    Site de filmes feito em React.
                   </span>
                 </div>
               </div>
@@ -69,24 +69,24 @@ const Work = () => {
                 <div className='absolute -bottom-full left-12 group-hover:bottom-14 
                 transition-all duration-700 z-50'>
                   <span className='text-3x1 text-white'>
-                    Blog pessoal sobre programação.
+                    Blog sobre programação feito em React.
                   </span>
                 </div>
               </div>
             </a>
 
-            <a href="https://alura-space-one-puce.vercel.app/" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/luquisinger/BibliotecaGenerica" target="_blank" rel="noopener noreferrer">
               <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
                 <div className='group-hover:bg-black/70 w-full h-full absolute z-40 
                 transition-all duration-300'></div>
                 <img className='group-hover:scale-125 transition-all duration-500'
                   src={Img3} alt=''></img>
                 <div className='absolute -bottom-full left-12 group-hover:bottom-24 
-                transition-all duration-500 z-50'><span className='text-gradient'>Alura Space</span></div>
+                transition-all duration-500 z-50'><span className='text-gradient'>Biblioteca</span></div>
                 <div className='absolute -bottom-full left-12 group-hover:bottom-14 
                 transition-all duration-700 z-50'>
                   <span className='text-3x1 text-white'>
-                    Galeria de imagens tiradas do espaço.
+                  CRUD em .net, SQL Server e Windows Forms.
                   </span>
                 </div>
               </div>
